0d46c3cf0bf76947e77cbecbb0356218dabb0e2e
JhonW67 <63158937+JhonW67@users.noreply.github.com>
2025-08-11 16:35:11 -0400

    feature: Heathcheck and bild application.properties

---
+++ backend/src/main/java/com/ProjectHub/interfaces/HeathController.java
@@ -0,0 +1,14 @@
+package src.main.java.com.ProjectHub.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController //indica que essa classe responde requisições REST
+public class HeathController {
+    @GetMapping("/health") //cria um endpoint HTTP GET na rota /health
+    public Map<String, String> health() {
+        //O método health() retorna um mapa com chave "status" e valor "ok".
+        return Map.of("status", "ok");
+    }
+}
