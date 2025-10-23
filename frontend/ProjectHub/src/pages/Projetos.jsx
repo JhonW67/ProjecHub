55d0465ae8d9c1f4f9c16c0b08c9c9525a7e1d2d
Aluno TI <alunoti@univaglabs.iemat>
2025-10-23 10:52:07 -0400

    Feat: Adiciona barra de pesquisa e botão de cadastrar projetos

--- frontend/ProjectHub/src/pages/Projetos.jsx
+++ frontend/ProjectHub/src/pages/Projetos.jsx
@@ -1,9 +1,22 @@
+import '../App.css'
 import React from 'react'
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
+
 
 const Projetos = () => {
   return (
     <div>
-      Projetos
+      <div className='search-bar'>
+        <IconField iconPosition="left">
+          <InputIcon className="pi pi-search"> </InputIcon>
+          <InputText placeholder="Buscar Projeto" className="search-input" />
+        </IconField>
+
+        <Button label="Novo Projeto" icon="pi pi-plus" className="add-project-button"/>
+      </div>
     </div>
   )
 }
